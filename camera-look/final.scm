9971943aff1c0507a037420f2986d79b94f5672e
Gabor Papp <gabor.papp@gmail.com>
2013-02-25 15:02:56 +0100

    some cleanup

--- camera-look/final.scm
+++ camera-look/final.scm
@@ -1,31 +1,60 @@
 (require lds/dof) ; os x: copy dof.ss to ~/Documents/Fluxus/collects/lds
-;(require "dof.ss")
-(require polyhedra/polyhedra)
+(require polyhedra/polyhedra) ; copy the polyhedra module to ~/Documents/collects/polyhedra
+;(require "dof.ss") ; linux: place the modules next to this script
+;(require "polyhedra.ss")
 
+; parameters to tune
+
+; tunnel parameters
 (define tunnel-inner-radius 11)
-(define tunnel-outer-radius 24)
-(define tunnel-slices 8)
-(define tunnel-stacks 18)
+(define tunnel-outer-radius 27)
+(define tunnel-slices 9)
+(define tunnel-stacks 27)
+
+(define polyhedron-id 3) ; polyhedra index from the polyhedra list below
+(define polyhedron-scale 4.0)
 
 (define clip-near .5) ; clip plane distances - change for fov
 (define clip-far 1000)
 
-(define pp-size 1024) ; pixel primitive size for rendering
+(define camera-distance 10.) ; distance of camera from the followed object
 
-(define ikopoints '())
-(define toruspoints '())
-(define all-points '())
-(define particle-sum 0)
-;//////////////// 
+; depth of field parameters
+(define dof-aperture .06) ; smaller value result in shallower blur range
+(define dof-focus .15) ; focus from 0. to 1, 0 is the camera position
+(define dof-maxblur 1.) ; maximum blur
+
+(define pp-size 1024) ; pixel primitive size for rendering - bigger gives better quality, but slower
+
+;------------------------------------------------------------------------------
+
+; list of polyhedron to choose from
+(define polyhedra '(medial-disdyakistriacontahedron
+                    great-dodecahemidodecahedron
+                    great-icosihemidodecahedron
+                    great-stellated-truncated-dodecahedron
+                    zonohedron-7-random
+                    tetradyakishexahedron
+                    stellated-icosahedron-5
+                    cubitruncated-cuboctahedron
+                    disdyakisdodecahedron
+                    great-cubicuboctahedron
+                    j86
+                    octagonal-dipyramid))
+
+;------------------------------------------------------------------------------
 
 (clear)
 
+; lock camera
 (set-camera-transform (mtranslate #(0 0 -10)))
 
+; create a render buffer for post processing (dof, bloom)
 (define render-buffer (build-pixels pp-size pp-size #t))
 (with-primitive render-buffer
     (scale 0))
 
+; returns the world position of the grabbed primitive
 (define (get-pos)
     (let ([t (get-global-transform)])
         (vector (vector-ref t 12)
@@ -44,12 +73,19 @@
             [camera-matrix (mmul orientation translation)])
         (set-camera camera-matrix)))
 
-;////////////////
+;------------------------------------------------------------------------------
+
 (define tunnel (with-pixels-renderer render-buffer
         (build-torus tunnel-inner-radius tunnel-outer-radius
             tunnel-slices tunnel-stacks)))
 
-(with-pixels-renderer render-buffer    
+; lists for particles and connector lines
+(define object-points '())
+(define tunnel-points '())
+(define all-points '())
+(define particle-sum 0)
+
+(with-pixels-renderer render-buffer
     (clip clip-near clip-far)
     (with-primitive tunnel
         (backfacecull 0)
@@ -61,54 +97,43 @@
         (poly-convert-to-indexed)
         (for ([i (in-range 0 (pdata-size) 1)])
             (let* ([c (pdata-ref "p" i)])
-                (set! toruspoints (cons c toruspoints)))
-            )
-        
-        ))
+                (set! tunnel-points (cons c tunnel-points))))))
 
-(define obj (with-pixels-renderer render-buffer
+; camera following the object
+(define camera-eye (with-pixels-renderer render-buffer (build-locator)))
+(define camera-target (with-pixels-renderer render-buffer
         (build-locator)))
 
-(define ico (with-pixels-renderer render-buffer
-        ;(build-icosphere 3)
-        
-        (build-polyhedron 'medial-disdyakistriacontahedron)
-        ))
+; object being followed
+(define object (with-pixels-renderer render-buffer
+        (build-polyhedron (list-ref polyhedra polyhedron-id))))
 
 (with-pixels-renderer render-buffer
-    (with-primitive ico
+    (with-primitive object
         (hint-sphere-map)
         (colour #(1 1 1))
-        (specular (vector .1 .1 1))    
+        (specular (vector .1 .1 1))
         (texture (load-texture "textures/colors.png"))
         (recalc-normals 0)
-        (poly-convert-to-indexed)
+        (scale polyhedron-scale)
+        (apply-transform)
         (for ([i (in-range 0 (pdata-size) 1)])
             (let ([c (pdata-ref "p" i)])
-                (set! ikopoints (cons c ikopoints)))
-            )
-        )
-    )
-(set! all-points (append ikopoints toruspoints))
+                (set! object-points (cons c object-points))))))
+
+(set! all-points (append object-points tunnel-points))
 (set! particle-sum (length all-points))
 
 (define particles (with-pixels-renderer render-buffer
         (with-state
-            (scale 1)
-            (build-particles particle-sum))
-        ))
+            (build-particles particle-sum))))
 
 (with-pixels-renderer render-buffer
-    (with-primitive particles 
+    (with-primitive particles
         (pdata-index-map!
-            (lambda (i c)
-                (vector 1 1 1 .0))
-            "c")
-        )
-    )
-
-(define cam (with-pixels-renderer render-buffer (build-locator)))
-(define pos 12)
+            (λ (i c) (vector 1 .1)) "c")
+        (pdata-index-map!
+            (λ (i s) .1) "s")))
 
 (define (mouse-look)
     (let* ([max tunnel-inner-radius]
@@ -121,15 +146,10 @@
     (rotate (vector 0 0 t))
     (translate (vector tunnel-outer-radius 0 0)))
 
-(define (destroyer) 
-    (set! ikopoints '())
-    )
-
-
 (define (draw-lines)
     (wire-colour (rgb->hsv (vector  (abs (sin (time))) (abs (cos (time))) (rndf) .9)))
-    (line-width (* (gl 0) 6))     
-    
+    (line-width (* (gl 0) 6))
+
     (with-primitive particles
         (for ([i (in-range 0 particle-sum 1)])
             (let ([v0  (pdata-ref "p" i)]
@@ -140,64 +160,60 @@
 (define (render-tunnel)
     (define up (vtransform (vector 0 0 1) (mrotate (vector 0 (* 20 (time)) 0))))
     (with-pixels-renderer render-buffer
-        (destroyer)
-        
-        (with-primitive ico 
+        (set! object-points '())
+
+        (with-primitive object
             (identity)
             (rotate (vector 0 0 (* (time) tunnel-outer-radius)))
             (translate (vector (+ (cos(time)) tunnel-outer-radius) (* 5 (abs (sin (time)))) (* 5 (sin (time)))))
-            (scale (+ (abs (sin (gl 0))) .5))    
-            
+            (scale (+ (abs (sin (gl 0))) .5))
+
             (for ([i (in-range 0 (pdata-size) 1)])
                 (let ([c (pdata-ref "p" i)])
-                    (set! ikopoints (cons c ikopoints)))
-                )
-            )
-        ;(set! all-points (append toruspoints ikopoints))
-        
-        (let ([m (with-primitive ico (get-global-transform))])
+                    (set! object-points (cons c object-points)))))
+
+        (let ([m (with-primitive object (get-global-transform))])
             (with-primitive particles
                 (pdata-index-map!
                     (λ (i p)
-                        (vtransform (with-primitive ico (pdata-ref "p" i)) m))
+                        (vtransform (with-primitive object (pdata-ref "p" i)) m))
                     "p")))
-        
-        
-        
-        (with-primitive particles 
-            (for ([i (in-range 0 (length toruspoints) 1)])
-                (let ([p (list-ref toruspoints i)])
-                    (pdata-set! "p" i p))
-                ))
-        
+
+
+        (with-primitive particles
+            (for ([i (in-range 0 (length tunnel-points) 1)])
+                (let ([p (list-ref tunnel-points i)])
+                    (pdata-set! "p" i p))))
+
         (draw-lines)
-        
-        (with-primitive obj
+
+        (with-primitive camera-target
             (identity)
-            (hint-origin)          
+            (hint-origin)
             (hide 1)
             (rot (* tunnel-outer-radius (time)))
             (when (mouse-button 2) (mouse-look)))
-        
-        (with-primitive cam 
+
+        (with-primitive camera-eye
             (identity)
-            (rot (- (* tunnel-outer-radius (time)) pos)))
-        
-        (set-target-camera (with-primitive cam (get-pos))
-            (with-primitive obj (get-pos))
-            up)))
+            (rot (- (* tunnel-outer-radius (time)) camera-distance)))
+
+        (set-target-camera (with-primitive camera-eye (get-pos))
+                           (with-primitive camera-target (get-pos))
+                           up)))
 
 
+; plane for post-processed output
 (define plane (build-plane))
 (with-primitive plane
     (scale #(21 17 1)))
 
-
 (define (loop)
     (render-tunnel)
     (with-primitive plane
-        (dof render-buffer #:aperture .06
-            #:focus (+ .2 (* .2 (sin (* .5 (time)))))
-            #:maxblur 2.5)))
+        (dof render-buffer #:aperture dof-aperture
+            #:focus dof-focus
+            #:maxblur dof-maxblur)))
 
 (every-frame (loop))
+
